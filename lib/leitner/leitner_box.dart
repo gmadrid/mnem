e8484809803b291fec8342f03daa3baf4c36283f
George Madrid <gmadrid@gmail.com>
2019-05-23 13:59:00 -0400

    Refactored LeitnerBox to have a dedicated 'waiting' and 'known' box. Active indices now number 0-6. Unit tests are all broken now.

--- lib/leitner/leitner_box.dart
+++ lib/leitner/leitner_box.dart
@@ -1,20 +1,21 @@
+import 'dart:math';
+
 import 'package:mnem/leitner/question.dart';
 import 'package:mnem/leitner/shuffler.dart';
 
 /// An implementation of the Leitner system (https://en.wikipedia.org/wiki/Leitner_system).
 ///
-/// The box has [last_bucket] buckets (numbered `1-[last_bucket]`) plus a "waiting
-/// bucket" (numbered `0`). In the language of the Leitner system, the questions
-/// in box 1 should be asked every session, those in box 2 should be asked every
+/// The box has [numBuckets] buckets (numbered `0-[numBuckets - 1]`) plus a "waiting
+/// bucket" and a "known bucket". In the language of the Leitner system, the questions
+/// in box 0 should be asked every session, those in box 1 should be asked every
 /// two sessions, and so on.
 ///
 /// As a question is answered correctly, it gets "promoted" to the next bucket.
 /// If a question is answered incorrectly, it gets "demoted" all the way back
 /// to the first bucket so that it can be refreshed in the user's memory.
 ///
-/// Once a question in [last_bucket] is answered correctly, it should graduate
-/// into the class of "known" questions which are never asked again. (NOTE: we
-/// don't currently do this.)
+/// Once a question in the last bucket is answered correctly, it should graduate
+/// into the class of "known" questions which are never asked again.
 ///
 /// This class simulates the physical box. In other words, it provides
 /// methods to _use_ the box (get the next question, promote or demote a
@@ -30,18 +31,30 @@ class LeitnerBox {
     for (var i = 0; i < _total_buckets; ++i) {
       _buckets.add(List());
     }
+    _known = List();
+    _waiting = List();
   }
+
+  /// The total number of Questions in all of the buckets including waiting and known.
+  num get totalSize => _numQuestions;
+
+  /// The number of Questions in the waiting bucket.
+  num get waitingSize => _waiting.length;
+
+  /// The number of Questions in the known bucket.
+  num get knownSize => _known.length;
+
+  /// The number of "active" Questions, that is [totalSize] - ([waitingSize] + [knownSize]).
+  num get activeSize => totalSize - (waitingSize + knownSize);
+
+  /// The number of buckets in this box.
+  num get numBuckets => _total_buckets;
 
   /// Adds questions to the "waiting bucket."
   void addQuestions(Iterable<Question> questions) {
-    _buckets[waiting_bucket].addAll(questions);
+    _waiting.addAll(questions);
     _numQuestions += questions.length;
-    this.shuffle(waiting_bucket);
-  }
-
-  /// The number of questions in all of the buckets.
-  num size() {
-    return _numQuestions;
+    this._shuffleList(_waiting);
   }
 
   /// The number of questions in a particular bucket.
@@ -69,10 +82,34 @@ class LeitnerBox {
   /// @param index The index of the bucket.
   ///     An out-of-range index will panic.
   void shuffle(num index) {
-    _shuffler.shuffle(_buckets[index]);
+    _shuffleList(_buckets[index]);
   }
 
-  /// Moves the last question in the specified bucket to bucket number `1`.
+  /// Shuffles the list.
+  ///
+  /// Uses the shuffler specified when this box was created.
+  ///
+  /// @param list The list to be shuffled.
+  void _shuffleList(List<Question> list) {
+    _shuffler.shuffle(list);
+  }
+
+  /// Moves questions from the 'waiting' list to the first bucket.
+  ///
+  /// If there are fewer than [numQuestions] questions in the waiting bucket,
+  /// then all questions in the waiting bucket will be moved.
+  ///
+  /// @param numQuestions The number of questions to move.
+  void makeActive(num numQuestions) {
+    var numToMove = min(numQuestions, waitingSize);
+    for (var i = 0; i < numToMove; ++i) {
+      var q = _waiting.last;
+      _waiting.removeLast();
+      _buckets.first.insert(0, q);
+    }
+  }
+
+  /// Demotes the last question in the specified bucket to the first bucket.
   ///
   /// This operation is useful in two contexts:
   ///   * A question was answered wrong and needs to be refreshed.
@@ -82,13 +119,13 @@ class LeitnerBox {
   ///
   /// @param index The index of the bucket.
   ///     An out-of-range index will panic.
-  void moveToFirst(num index) {
+  void demote(num index) {
     if (bucketSize(index) < 1) return;
 
     var fromBucket = _buckets[index];
     var q = fromBucket.last;
     fromBucket.removeLast();
-    _buckets[first_bucket].insert(0, q);
+    _buckets[_first_bucket].insert(0, q);
   }
 
   /// Moves the last question in the specifed bucket into the `[index] + 1`
@@ -104,25 +141,27 @@ class LeitnerBox {
   ///
   /// @param index The index of the bucket.
   ///     An out-of-range index will panic.
-  void moveUp(num index) {
+  void promote(num index) {
     if (bucketSize(index) < 1) return;
-
-    // TODO: have a way to deal with this case.
-    if (index == last_bucket) return;
 
     var fromBucket = _buckets[index];
     var q = fromBucket.last;
     fromBucket.removeLast();
-    _buckets[index + 1].insert(0, q);
+
+    if (index == _last_bucket) {
+      _waiting.add(q);
+    } else {
+      _buckets[index + 1].insert(0, q);
+    }
   }
 
-  static const num first_bucket = 1;
-  static const num last_bucket = 7;
-  static const num waiting_bucket = 0;
-
-  static const num _total_buckets = last_bucket + 1;
+  static const num _first_bucket = 0;
+  static const num _last_bucket = 6;
+  static const num _total_buckets = _last_bucket + 1;
 
   Shuffler _shuffler;
   List<List<Question>> _buckets;
+  List<Question> _waiting;
+  List<Question> _known;
   num _numQuestions = 0;
 }

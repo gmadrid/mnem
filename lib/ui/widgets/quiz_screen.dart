552937bed9dabe4ba5186934e1d9dcc759ee4d7a
George Madrid <gmadrid@gmail.com>
2019-05-29 07:58:32 -0400

    Conflicted.

--- lib/ui/widgets/quiz_screen.dart
+++ lib/ui/widgets/quiz_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mnem/ui/widgets/keyboards/playing_card.dart';
 
 class QuizScreen extends StatelessWidget {
   @override
@@ -10,6 +11,29 @@ class QuizScreen extends StatelessWidget {
           appBar: AppBar(
             title: Text("Mnem"),
           ),
+<<<<<<< Updated upstream
+=======
+          body: Column(
+            children: <Widget>[
+              Container(
+                color: Color.fromRGBO(255, 0, 0, 1.0),
+                child: Text("foo"),
+              ),
+              Container(
+                color: Color.fromRGBO(0, 255, 0, 1.0),
+                child: Text("bar"),
+              ),
+              Container(
+                color: Color.fromRGBO(255, 255, 0, 1.0),
+                child: Text("quux"),
+              ),
+              Container(
+                color: Color.fromRGBO(255, 0, 255, 1.0),
+                child: PlayingCardKeyboard(textStyle: TextStyle()),
+              ),
+            ],
+          ),
+>>>>>>> Stashed changes
         ));
   }
 
